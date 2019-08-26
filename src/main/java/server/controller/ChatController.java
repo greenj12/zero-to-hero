705f0d3fa85d0d608d196f38384413de26792e74
Jon Green <jgreen@activecampaign.com>
2019-08-26 14:29:19 -0500

    Added maven wrapper, React, Node

--- src/main/java/server/controller/ChatController.java
+++ src/main/java/server/controller/ChatController.java
@@ -3,7 +3,6 @@ package server.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 import server.model.APIRequest;
 import server.model.APIRequestType;
@@ -15,7 +14,6 @@ public class ChatController {
     @MessageMapping("/chat/global")
     @SendTo("/topic/chat/global")
     public APIResponse sendGlobalMessage(APIRequest message) throws Exception {
-
         return new APIResponse(message.getContent());
     }
 }
--- src/main/java/server/controller/HomeController.java
+++ src/main/java/server/controller/HomeController.java
@@ -1,1 +1,13 @@
 package server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+}
